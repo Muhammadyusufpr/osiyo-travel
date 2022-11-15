b1153e6bac5bb9dadbec933e8ac59793d2c327e0
muhammadyusuf <muhammadyusufgapirov@gmail.com>
2022-11-15 14:46:27 +0500

    Initial commit

---
+++ src/main/java/com/osiyotravel/OsiyoTravelApplication.java
@@ -0,0 +1,13 @@
+package com.osiyotravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsiyoTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsiyoTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osiyotravel/osiyotravel/OsiyoTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiyotravel.osiyotravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsiyoTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
